b23e2423071e7a84f76f0aafbd750be34fe8f495
Antoine Gagnon <antoine.gagnon760@gmail.com>
2022-01-15 21:30:34 -0500

    Clean up default Remix app, add some starter votify stuff

--- web/app/routes/vote.tsx
+++ web/app/routes/vote.tsx
@@ -3,9 +3,7 @@ import { Link } from "remix";
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Votify</h1>
-      <h2>Landing Page</h2>
-      <Link to="/vote">Vote now</Link>
+      <h1>Vote</h1>
     </div>
   );
 }
