a729fbab06a3144042554656ccbfd4b0f840d4ad
Antoine Gagnon <antoine.gagnon760@gmail.com>
2022-01-27 15:23:25 -0500

    Fix hot reload causing multiple Prisma instances to be created

--- web/app/lib/prisma.ts
+++ web/app/lib/prisma.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  // @ts-ignore
+  if (!global.prisma) {
+    // @ts-ignore
+    global.prisma = new PrismaClient();
+  }
+  // @ts-ignore
+  prisma = global.prisma;
+}
+
+export default prisma;
