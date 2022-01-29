6a614f0e6eadc63dbac3e6fbbd282313210dca1a
Antoine Gagnon <antoine.gagnon760@gmail.com>
2022-01-28 20:11:36 -0500

    (Test page) Implement form to add a user with a specific ID

--- web/app/lib/users.ts
+++ web/app/lib/users.ts
@@ -5,6 +5,11 @@ export async function getUsers() {
   const users = await prisma.user.findMany();
 
   // Transform data if needed
-
   return users;
+}
+
+export async function createUser(userId: number) {
+  const user = await prisma.user.create({ data: { id: userId } });
+
+  return user;
 }
--- web/app/routes/users.tsx
+++ web/app/routes/users.tsx
@@ -1,9 +1,33 @@
-import { Box, Button, List, ListItemText, Typography } from "@mui/material";
-import { useLoaderData } from "remix";
-import { getUsers } from "../lib/users";
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ActionFunction, Form, useLoaderData } from "remix";
+import { createUser, getUsers } from "../lib/users";
 
 export const loader = async () => {
   return await getUsers();
+};
+
+// https://remix.run/docs/en/v1/guides/data-writes
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+
+  try {
+    const id = parseInt(body.get("user_id")?.toString() || "");
+    if (id) {
+      const user = await createUser(id);
+      return { values: { user: user } };
+    }
+  } catch (error) {
+    return { errors: { user_id: "User ID has to be a number" } };
+  }
 };
 
 export default function Users() {
@@ -13,22 +37,37 @@ export default function Users() {
     <Box>
       <Typography variant="h1">Welcome to Votify</Typography>
       <Typography variant="h2">This is a test page :)</Typography>
-      <Button
-        variant="contained"
-        sx={{
-          mt: 4,
-        }}
-      >
-        Insert a new user
-      </Button>
+      <Form method="post">
+        <Stack sx={{ mt: 4 }}>
+          <TextField
+            id="user_id"
+            name="user_id"
+            label="User ID"
+            variant="outlined"
+            size="small"
+            type="number"
+          />
+          <Button
+            variant="contained"
+            sx={{
+              mt: 4,
+            }}
+            type="submit"
+          >
+            Insert a new user
+          </Button>
+        </Stack>
+      </Form>
       <Typography variant="h2" sx={{ mt: 4 }}>
         Users:
       </Typography>
       <List>
-        {users ??
-          users.map((user: string) => {
-            <ListItemText primary={user} />;
-          })}
+        {users &&
+          users.map((user: { id: string }) => (
+            <ListItem>
+              <ListItemText primary={user.id} />;
+            </ListItem>
+          ))}
       </List>
     </Box>
   );
