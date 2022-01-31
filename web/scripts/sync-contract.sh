191689a4819c2bb8232693deb2d9a591150d1448
Victor Cheeney <victorcheeney@gmail.com>
2022-01-31 15:05:33 -0500

    Added a script to move/copy the compiled contract from the hardhat project to the web project

---
+++ web/scripts/sync-contract.sh
@@ -0,0 +1,17 @@
+# Give proper warning
+echo "⚠️  FRIENDLY WARNING 😊"
+echo "Make sure you have already compiled the contract in the hardhat project"
+echo "See the README at the root directory for instructions"
+echo ""
+
+# Create the compiled contracts directory in the web project
+mkdir -p contracts
+
+# Copy the compiled contract
+cp -r ../hardhat/artifacts/contracts/Ballot.sol/Ballot.json contracts/Ballot.json
+
+# Generate the types
+npx typechain --target=ethers-v5 "contracts/Ballot.json"
+
+echo ""
+echo "Done ✅"
