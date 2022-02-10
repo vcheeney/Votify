fca781d3634141585998c99593d5939c8fa4c8d7
Victor Cheeney <victorcheeney@gmail.com>
2022-02-10 13:09:56 -0500

    Removed hardcoded address

--- hardhat/scripts/giveRightToVote.ts
+++ hardhat/scripts/giveRightToVote.ts
@@ -3,9 +3,7 @@ import { ethers } from "hardhat";
 async function main() {
   const [chairperson, ...voters] = await ethers.getSigners();
 
-  const address =
-    process.env.CONTRACT_ADDRESS ||
-    "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const address = process.env.CONTRACT_ADDRESS!;
 
   const ballot = await ethers.getContractAt("Ballot", address);
 
