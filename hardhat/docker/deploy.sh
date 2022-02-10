19b795eb8621c4cd7ec9ada6e6de2ca32808a905
Victor Cheeney <victorcheeney@gmail.com>
2022-02-10 17:04:33 -0500

    Think I got it

---
+++ hardhat/docker/deploy.sh
@@ -0,0 +1,14 @@
+n=1
+SLEEP=5
+MAX_ATTEMPTS=10
+
+until [ "$n" -ge $MAX_ATTEMPTS ]
+do
+   echo "📜 Trying to deploy the smart contract... (Attempt $n/$MAX_ATTEMPTS)";
+   exec 2> /dev/null
+   npm run deploy && echo "📜 Deployment succeeded! 🎉" && break;
+   exec 2>&1
+   n=$((n+1));
+   echo "📜 Deployment failed, retrying in $SLEEP seconds...";
+   sleep $SLEEP;
+done
--- hardhat/docker/entrypoint.sh
+++ hardhat/docker/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+echo "🔗📜 Launching the local blockchain and deploying the smart contract...";
+
 # Change to the correct directory
 cd /usr/src/blockchain;
-# Run hardhat
+
+# Start the smart contract deployment script
+# (Will retry every X seconds until it succeeds or Y attempts are made)
+# See the X and Y values in the script itself
+sh docker/deploy.sh &  PIDDEPLOY=$!
+
+# Run the hardhat local blockchain
 npm run node
-# Keep node alive
-set -e
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- node "$@"
-fi
-exec "$@"
