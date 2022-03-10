1f113e26f8652ca8d236d3a5f546b9af63668683
Victor Cheeney <victorcheeney@gmail.com>
2022-03-10 15:13:38 -0500

    🤦‍♂️

--- web/app/context/BallotContext.tsx
+++ web/app/context/BallotContext.tsx
@@ -131,7 +131,7 @@ export const BallotProvider: FC = ({ children }) => {
   }, [ethereumLoading, ethereumExists, network]);
 
   useEffect(() => {
-    if (!loading) {
+    if (loading) {
       return;
     }
 
